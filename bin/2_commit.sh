d729e0620aa6b229e04890e13953746f94a9557b
matxx <matxx.webmaster@gmail.com>
2025-08-08 17:06:46 +0200

    feat: push to origin in script

--- bin/2_commit.sh
+++ bin/2_commit.sh
@@ -4,5 +4,5 @@ require 'date'
 
 if `git status --porcelain`.split("\n").any? { |line| line.include?('data/') }
   `git commit data/* -m "chore: #{Date.today.strftime('%Y-%m-%d')} update"`
-  # `git push origin`
+  `git push origin`
 end
